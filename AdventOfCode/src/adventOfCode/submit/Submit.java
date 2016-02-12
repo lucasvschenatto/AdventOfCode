930f15f271613498dce103c15ba0b6a687511db0
I841311 <I841311@SLEN00543697A.amer.global.corp.sap>
2016-02-12 18:05:20 -0200

    a

--- AdventOfCode/src/adventOfCode/submit/Submit.java
+++ AdventOfCode/src/adventOfCode/submit/Submit.java
@@ -11,7 +11,7 @@ public class Submit {
 		while(true){
 			String userChoice = "";
 			for(boolean valid = false;valid==false;){
-				userChoice = JOptionPane.showInputDialog("Enter challenge number (1-25)\nNo number exits application");
+				userChoice = JOptionPane.showInputDialog("Enter challenge number (1-25)\nNot a number exits application");
 				try{
 					Integer.parseInt(userChoice);
 					valid = true;
