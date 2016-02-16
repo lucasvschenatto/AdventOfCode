52bfcf2dd98b1e57883bb32b1853e4f74b5a70b9
I841311 <I841311@SLEN00543697A.amer.global.corp.sap>
2016-02-16 11:56:01 -0200

    Day6 part2 complete

--- AdventOfCode/src/adventOfCode/Day6.java
+++ AdventOfCode/src/adventOfCode/Day6.java
@@ -3,26 +3,38 @@ package adventOfCode;
 import java.util.Arrays;
 import java.util.List;
 
-import adventOfCode.Day5.StrategySet;
-
 public class Day6 implements Challenge{
-	public int countTurnedOn(String commands) {
-		Grid grid = new Grid(1000,1000);
+	private Grid grid;
+	final int gridSize = 1000;
+	public Day6(){
+		grid = new Grid(gridSize,gridSize);
+	}
+	public void activateBrightnessControl(){
+		grid = new Grid(gridSize,gridSize){
+			@Override
+			public void turnOn(int x, int y) {
+				light[x][y]++;
+			}
+			@Override
+			public void turnOff(int x, int y) {
+				light[x][y] = (light[x][y]==0)? 0: light[x][y]-1;
+			}
+			@Override
+			public void toogle(int x, int y) {
+				light[x][y] = light[x][y]+2;
+			}
+		};
+	}
+	public void configureGrid(String commands) {
 		List<String> commandList = Arrays.asList(commands.split("\n"));
 		for (String command : commandList) {
 			String[] token = getTokens(command);
 			Command c = interpret(token[0]);
 			c.execute(grid, token[1],token[2]);
 		}		
-		return grid.countLightsOn();
 	}
-	public int countTurnedOff(String commands) {
-		Grid grid = new Grid(1000,1000);
-		grid.turnAllOn();
-		String[] token = getTokens(commands);
-		Command c = interpret(token[0]);
-		c.execute(grid, token[1],token[2]);
-		return 1000000 - grid.countLightsOn();
+	public int countLightsOn(){
+		return grid.countLightsOn();
 	}
 	private Command interpret(String instruction){
 		Command c = Command.valueOf(instruction.toUpperCase());
@@ -36,21 +48,13 @@ public class Day6 implements Challenge{
 		return token;
 	}
 	private String getInstructionName(String command) {
-		if(startsWithTwoWords(command))
-			return firstTwo(command);
+		String[] token = command.split(" ");
+		if(startsWithTwoWords(token))
+			return token[0] + "_" + token[1];
 		else
-			return firstOne(command);
+			return token[0];
 	}
-	private String firstOne(String command) {
-		String[] token = command.split(" ");
-		return token[0];
-	}
-	private String firstTwo(String command) {
-		String[] token = command.split(" ");
-		return token[0] + "_" + token[1];
-	}
-	private boolean startsWithTwoWords(String command) {
-		String token[] = command.split(" ");
+	private boolean startsWithTwoWords(String[] token) {
 		if (token.length >= 2)
 			if (!token[1].contains(","))
 				return true;
@@ -72,32 +76,36 @@ public class Day6 implements Challenge{
 		return "";
 	}
 	class Grid{
-		boolean [][] light;
+		protected int [][] light;
 		Grid(int x, int y){
-			light = new boolean[x][y];
+			light = new int[x][y];
 		}
-		public void turnAllOn() {
-			for (int i = 0; i < light.length; i++)
-				for (int j = 0; j < light[i].length; j++)
-					light[i][j] = true;
+		public void turnOn(int x, int y){
+			light[x][y] = 1;
+		}
+		public void turnOff(int x, int y){
+			light[x][y] = 0;
+		}
+		public void toogle(int x, int y){
+			light[x][y] = (light[x][y] == 0)? 1:0;
 		}
 		int countLightsOn(){
 			int count = 0;
 			for (int i = 0; i < light.length; i++)
 				for (int j = 0; j < light[i].length; j++)
-					if (light[i][j]) count++;
+					count = count + light[i][j];
 			return count;
 		}
 	}
 	private enum Command{
 		TURN_ON( (grid, x, y)->{
-			grid.light[x][y] = true;
+			grid.turnOn(x,y);
 		}),
 		TURN_OFF((grid, x, y)->{
-			grid.light[x][y] = false;
+			grid.turnOff(x,y);
 		}),
 		TOGGLE((grid, x, y)->{
-			grid.light[x][y] = (grid.light[x][y])? false:true;
+			grid.toogle(x,y);
 		}),
 		DUMMY((grid, x, y)->{});
 		private Action action;
@@ -119,10 +127,15 @@ public class Day6 implements Challenge{
 	}
 	@Override
 	public String part1(String input) {
-		return String.valueOf(new Day6().countTurnedOn(input));
+		Day6 d = new Day6();
+		d.configureGrid(input);
+		return String.valueOf(d.countLightsOn());
 	}
 	@Override
 	public String part2(String input) {
-		return String.valueOf(new Day6().countTurnedOn(input));
+		Day6 d = new Day6();
+		d.activateBrightnessControl();
+		d.configureGrid(input);
+		return String.valueOf(d.countLightsOn());
 	}
 }
--- AdventOfCode/test/adventOfCode/Day6Test.java
+++ AdventOfCode/test/adventOfCode/Day6Test.java
@@ -6,55 +6,94 @@ import org.junit.Test;
 
 public class Day6Test {
 
-	
-	public static class Simple extends Day6Test{
-		@Test
-		public void turnOn(){
-			lightsOnFor("turn on 9,0",1);
-			lightsOnFor("turn on 456,753",1);
+	public static class part1{
+		public static class Simple extends Day6Test{
+			@Test
+			public void turnOn(){
+				lightsOnFor("turn on 9,0",1);
+				lightsOnFor("turn on 456,753",1);
+			}
+			@Test
+			public void turnOff(){
+				lightsOffFor("turn off 2,2",1);
+				lightsOffFor("turn off 643,865",1);
+			}
+			@Test
+			public void toogle(){
+				lightsOnFor("toggle 0,0",1);
+				lightsOffFor("toggle 0,0",1);
+			}
 		}
-		@Test
-		public void turnOff(){
-			lightsOffFor("turn off 2,2",1);
-			lightsOffFor("turn off 643,865",1);
-		}
-		@Test
-		public void toogle(){
-			lightsOnFor("toggle 0,0",1);
-			lightsOffFor("toggle 0,0",1);
+		public static class Range extends Day6Test{
+			@Test
+			public void turnOn(){
+				lightsOnFor("turn on 0,0 through 999,999",1000000);
+				lightsOnFor("turn on 0,0 through 999,0",1000);
+			}
+			@Test
+			public void turnOff(){
+				lightsOffFor("turn off 499,499 through 500,500",4);
+				lightsOffFor("turn off 20,0 through 49,0",30);
+			}
+			@Test
+			public void toogle(){
+				lightsOnFor("toggle 0,0 through 999,0", 1000);
+				lightsOffFor("toggle 0,0 through 999,0", 1000);
+			}
+			@Test
+			public void manyCommands(){
+				lightsOnFor("turn on 0,0 through 8,0"
+						  + "\nturn off 2,0 through 3,0"
+						  + "\ntoggle 7,0 through 10,0", 7);
+			}
 		}
 	}
-	public static class Range extends Day6Test{
-		@Test
-		public void turnOn(){
-			lightsOnFor("turn on 0,0 through 999,999",1000000);
-			lightsOnFor("turn on 0,0 through 999,0",1000);
+	public static class part2{
+		public static class range extends Day6Test{
+			@Test
+			public void turnOn(){
+				brightnessControlLightsOnFor("turn on 0,0 through 999,999",1000000);
+				brightnessControlLightsOnFor("turn on 0,0 through 999,0",1000);
+			}
+			@Test
+			public void turnOff(){
+				brightnessControlLightsOnFor("turn on 0,0 through 999,999"
+						+ "\nturn off 499,499 through 500,500",1000000 - 4);
+				brightnessControlLightsOnFor("turn on 0,0 through 999,999"
+						+ "\nturn off 20,0 through 49,0",1000000 - 30);
+			}
+			@Test
+			public void toogle(){
+				brightnessControlLightsOnFor("toggle 0,0 through 999,0", 2000);
+				brightnessControlLightsOnFor("turn on 0,0 through 999,999"
+						+ "\ntoggle 0,0 through 999,0", 1002000);
+			}
+			@Test
+			public void manyCommands(){
+				brightnessControlLightsOnFor("turn on 0,0 through 8,0"
+						  + "\nturn off 2,0 through 3,0"
+						  + "\ntoggle 7,0 through 10,0", 15);
+			}
 		}
-		@Test
-		public void turnOff(){
-			lightsOffFor("turn off 499,499 through 500,500",4);
-			lightsOffFor("turn off 20,0 through 49,0",30);
-		}
-		@Test
-		public void toogle(){
-			lightsOnFor("toggle 0,0 through 999,0", 1000);
-			lightsOffFor("toggle 0,0 through 999,0", 1000);
-		}
-		@Test
-		public void manyCommands(){
-			lightsOnFor("turn on 0,0 through 8,0"
-					  + "\nturn off 2,0 through 3,0"
-					  + "\ntoggle 7,0 through 10,0", 7);
-		}
+	}
+	protected void brightnessControlLightsOnFor(String commands, int expected) {
+		Day6 d = new Day6();
+		d.activateBrightnessControl();
+		d.configureGrid(commands);
+		int actual = d.countLightsOn();
+		assertEquals(expected,actual);
 	}
 	protected void lightsOnFor(String commands, int expected) {
 		Day6 d = new Day6();
-		int actual = d.countTurnedOn(commands);
+		d.configureGrid(commands);
+		int actual = d.countLightsOn();
 		assertEquals(expected,actual);
 	}
 	protected void lightsOffFor(String commands, int expected) {
 		Day6 d = new Day6();
-		int actual = d.countTurnedOff(commands);
+		d.configureGrid("turn on 0,0 through 999,999");
+		d.configureGrid(commands);
+		int actual = 1000000- d.countLightsOn();
 		assertEquals(expected,actual);
 	}
 }
