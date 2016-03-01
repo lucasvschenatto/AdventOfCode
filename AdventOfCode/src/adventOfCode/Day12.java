0e608a3c6a4f37ff0a12d510a6814772ff6b434d
I841311 <I841311@SLEN00543697A.amer.global.corp.sap>
2016-03-01 18:06:26 -0300

    Day12 part2 incomplete

--- AdventOfCode/src/adventOfCode/Day12.java
+++ AdventOfCode/src/adventOfCode/Day12.java
@@ -3,24 +3,31 @@ package adventOfCode;
 import adventOfCode.submit.Challenge;
 
 public class Day12 implements Challenge {
-
+	private final String NUMBERS = "[[\\W]&&[^-]]";
+	private final String NONREDS = "[[\\W]&&[^-]|[{.*\"red\".*}]";
 	@Override
 	public String part1(String input) {
-		return "";
+		String result = String.valueOf(sumAllNumbers(input));
+		return result;
 	}
-
 	@Override
 	public String part2(String input) {
-		return "";
+		String result = String.valueOf(sumAllNonReds(input));
+		return result;
 	}
-
-	public int sumAll(String jSON) {
-		for (String element : jSON.split("[[-]]")) {
+	public int sumAllNumbers(String jSON){
+		return sumAllOf(jSON, NUMBERS);
+	}
+	public int sumAllNonReds(String jSON){
+		return sumAllOf(jSON, NONREDS);
+	}
+	public int sumAllOf(String jSON, String regex) {
+		int sum = 0;
+		for (String element : jSON.split(regex)) {
 			try{
-				return Integer.parseInt(element);				
+				sum+= Integer.parseInt(element);
 			}catch(NumberFormatException e){}
 		}
-		return 0;
+		return sum;
 	}
-
 }
--- AdventOfCode/test/adventOfCode/Day12Test.java
+++ AdventOfCode/test/adventOfCode/Day12Test.java
@@ -7,14 +7,36 @@ import org.junit.Test;
 public class Day12Test {
 
 	@Test
-	public void test() {
-		SumFor("[]",0);
-		SumFor("[1]",1);
+	public void sumNumbers() {
+		sumNumbersFor("",0);
+		sumNumbersFor("[]",0);
+		sumNumbersFor("{}",0);
+		sumNumbersFor("[1]",1);
+		sumNumbersFor("[1,2]",3);
+		sumNumbersFor("[1,2,3]",6);
+		sumNumbersFor("[10,20,30]",60);
+		sumNumbersFor("{\"a\":2,\"b\":4}",6);
+		sumNumbersFor("[[[3]]]",3);
+		sumNumbersFor("{\"a\":{\"b\":4},\"c\":-1}",3);
+		sumNumbersFor("[{\"a\":[-1,1]}]",0);
+		sumNumbersFor("[-1,{\"a\":1}]",0);
+	}
+	@Test
+	public void sumNonReds(){
+		sumNonRedsFor("",0);
+		sumNonRedsFor("[1,2,3]",6);
+//		sumNonRedsFor("[10,20,30]",60);
+		
 	}
 
-	private void SumFor(String JSON, int expected) {
+	private void sumNonRedsFor(String jSON, int expected) {
 		Day12 d = new Day12();
-		int actual = d.sumAll(JSON);
+		int actual = d.sumAllNonReds(jSON);
+		assertEquals(expected, actual);
+	}
+	private void sumNumbersFor(String jSON, int expected) {
+		Day12 d = new Day12();
+		int actual = d.sumAllNumbers(jSON);
 		assertEquals(expected, actual);
 	}
 
