007ef2aea32a4e84e31adeb2aea314d499fc3c2d
I841311 <I841311@SLEN00543697A.amer.global.corp.sap>
2016-03-19 08:57:57 -0300

    .

--- AdventOfCode/src/adventOfCode/day15/RecipeMaker.java
+++ AdventOfCode/src/adventOfCode/day15/RecipeMaker.java
@@ -32,26 +32,24 @@ public class RecipeMaker {
 	private void tryToImprove() {
 		Spoon[] a = new Spoon[0];
 		a = (Spoon[]) arrangement.values().toArray(a);
-		for (int i = 0; i < a.length; i++) {
-			for (int j = 0; j< a.length;j++){
+		for (int i = 0; i < a.length; i++) 
+			for (int j = 0; j< a.length;j++)
 				if (i != j){
 					improveThruTransfer(a, i, j);
 					improveThruTransfer(a, j, i);
 				}
-			}
-		}
 	}
 	
 	private void improveThruTransfer(Spoon[] spoons, int from, int to){
-		boolean decrease = true;
-		while(decrease && !spoons[from].isEmpty()){
+		boolean transfer = true;
+		while(transfer && !spoons[from].isEmpty()){
 			int before = recipe.totalScore();
 			spoons[from].quantity--;
 			spoons[to].quantity++;
 			recipe.newQuantities(arrangement);
 			int after = recipe.totalScore();
 			if(before>after || spoons[from].quantity==0){
-				decrease = false;
+				transfer = false;
 				spoons[from].quantity++;
 				spoons[to].quantity--;
 				recipe.newQuantities(arrangement);
